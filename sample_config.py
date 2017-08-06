ec662ce510e26885688aed11543c8614108bd20d
Matt Lewis <matt@mplewis.com>
2017-08-06 15:26:21 -0700

    config: parse sheet id from url

--- sample_config.py
+++ sample_config.py
@@ -1,3 +1,4 @@
+import re
 from os import environ
 
 # URL to the Groupme API endpoint
@@ -7,9 +8,11 @@ API_URL = 'https://api.groupme.com/v3/bots/post'
 SHEET_BASE_URL = ('https://spreadsheets.google.com/feeds/cells/%s/%s'
                   '/public/values?alt=json')
 
+SHEET_ID_MATCHER = r'spreadsheets\/d\/(.+)\/'
+
 # Set by Heroku variables
 COMMAND_PREFIX = environ['BOT_NAME'] + ' '
 BOT_ID = environ['GROUPME_BOT_ID']
-SHEET_KEY = '0Ap_Q66-EH4RtdER3VVUwQUs4UmwyNXJtYWNGLUxJYVE'
 WORKSHEET_KEYS_TO_SCRAPE = environ['WORKSHEET_KEYS'].split(',')
 INDEX_REDIRECT_URL = 'https://github.com/mplewis/trianglebot'
+SHEET_KEY = re.search(SHEET_ID_MATCHER, environ['DOCUMENT_URL']).group(1)
