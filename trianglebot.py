9cab393908ea2202cf77de165fddb151ecdc842e
Matt Lewis <matt@mplewis.com>
2017-08-06 15:18:09 -0700

    heroku: move config to env

--- trianglebot.py
+++ trianglebot.py
@@ -15,7 +15,7 @@ app = Flask(__name__)
 def send_to_group(message):
     headers = {'content-type': 'application/json; charset=utf-8'}
     to_api = json.dumps({'bot_id': config.BOT_ID, 'text': message})
-    resp = requests.post(config.API_URL, to_api, headers=headers)
+    requests.post(config.API_URL, to_api, headers=headers)
 
 
 def summary_for_name(name):
